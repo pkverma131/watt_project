c2c5f8d6b6c3d83b53840a3165255178c5e672b1
Pradeep Verma <pkverma131@gmail.com>
2024-05-27 18:52:09 +0000

    SiteVisitor reporter

--- acwatts_backend/report/views.py
+++ acwatts_backend/report/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.db.models import Sum
+from django.db.models import Count, Sum
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from .models import SiteVisitor
@@ -9,16 +9,16 @@ def site_statistics_view(request):
     if not request.user.is_superuser:
         return HttpResponseForbidden()
 
-    # Total visits to the website
-    total_visits_to_website = SiteVisitor.objects.aggregate(total_hits=Sum('hits'))['total_hits'] or 0
+    # Total visits to the website (sum of hits across all paths)
+    total_visits_to_website = SiteVisitor.objects.aggregate(total_hits=Sum('hits')).get('total_hits') or 0
 
-    # Total unique visits to the website
-    total_unique_visits_to_website = SiteVisitor.objects.aggregate(total_unique_visits=Sum('unique_visits'))['total_unique_visits'] or 0
+    # Total unique visits to the website (count distinct IP addresses)
+    total_unique_visits_to_website = SiteVisitor.objects.aggregate(total_unique_visits=Count('ip_address')).get('total_unique_visits') or 0
 
     # Path-wise total visits and unique visits
     path_statistics = SiteVisitor.objects.values('path').annotate(
         total_hits=Sum('hits'),
-        total_unique_visits=Sum('unique_visits')
+        total_unique_visits=Count('ip_address', distinct=True)
     )
 
     context = {
