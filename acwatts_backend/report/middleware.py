f1361c010aaefc42d90313ef44c6278714df27af
Pradeep Verma <pkverma131@gmail.com>
2024-05-27 16:34:08 +0000

    exclude admin

--- acwatts_backend/report/middleware.py
+++ acwatts_backend/report/middleware.py
@@ -2,24 +2,33 @@ from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
 from .models import SiteVisitor
 
-class StatCounterMiddleware(MiddlewareMixin):
+class SiteVisitorMiddleware(MiddlewareMixin):
     def process_request(self, request):
         # Check if the current path should be excluded from tracking
-        if request.path_info in getattr(settings, 'STAT_COUNTER_EXCLUDE_URLS', []):
+        if request.path_info.startswith('/admin/') or request.path_info in getattr(settings, 'STAT_COUNTER_EXCLUDE_URLS', []):
             return None
 
-        # Get the visitor's IP address
-        ip_address = request.META.get('REMOTE_ADDR')
+        # Get client IP address
+        ip_address = self.get_client_ip(request)
 
         # Update or create a SiteVisitor entry for the current path and IP address
         site_visitor, created = SiteVisitor.objects.get_or_create(path=request.path_info, ip_address=ip_address)
         site_visitor.hits += 1
-
-        # Increment unique_visits if it's a new visitor
         if created:
             site_visitor.unique_visits += 1
-
+        else:
+            if not SiteVisitor.objects.filter(path=request.path_info, ip_address=ip_address).exists():
+                site_visitor.unique_visits += 1
         site_visitor.save()
 
         # Continue processing the request
         return None
+
+    def get_client_ip(self, request):
+        # Method to retrieve the client's IP address from the request
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
