da470b6eb5af33541a5a70368c7d3eaeb979f09c
Pradeep Verma <pkverma131@gmail.com>
2024-05-27 20:27:13 +0000

    tracking admin

--- acwatts_backend/report/admin.py
+++ acwatts_backend/report/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SiteVisitor
+from .models import SiteVisitor, SessionData
 
 class SiteVisitorAdmin(admin.ModelAdmin):
     list_display = ('path', 'ip_address', 'hits', 'unique_visits','last_visit')
@@ -7,4 +7,11 @@ class SiteVisitorAdmin(admin.ModelAdmin):
     list_filter = ('path',)
     ordering = ('-hits',)
 
+class SessionDataAdmin(admin.ModelAdmin):
+    list_display = ('session_key', 'ip_address', 'created_at')
+    search_fields = ('session_key', 'ip_address')
+    readonly_fields = ('session_key', 'ip_address', 'created_at')
+
+
 admin.site.register(SiteVisitor, SiteVisitorAdmin)
+admin.site.register(SessionData, SessionDataAdmin)
