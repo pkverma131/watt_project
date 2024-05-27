2ca8f16cd30e7612f9945db776f30a4207bf0385
Pradeep Verma <pkverma131@gmail.com>
2024-05-27 21:28:54 +0000

    add important specifications in product listing page

--- acwatts_backend/catalogue/serializers.py
+++ acwatts_backend/catalogue/serializers.py
@@ -7,11 +7,21 @@ class ImportantSpecificationSerializer(serializers.ModelSerializer):
         fields = ('wattage', 'coverage_area', 'energy_rating', 'default_bill_amount')
 
 class ProductSerializer(serializers.ModelSerializer):
-    important_specification = ImportantSpecificationSerializer()
+    important_specification = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         exclude = ('model_number', 'thumbnail', 'created_by', 'updated_by', 'brand')
+    
+    def get_important_specification(self, obj):
+        # Retrieve the related ImportantSpecification for the current Product instance
+        try:
+            important_specification = obj.important_specification
+            # Serialize the ImportantSpecification data
+            serializer = ImportantSpecificationSerializer(important_specification)
+            return serializer.data
+        except ImportantSpecification.DoesNotExist:
+            return None
 
 class BillAmountSerializer(serializers.ModelSerializer):
     class Meta:
