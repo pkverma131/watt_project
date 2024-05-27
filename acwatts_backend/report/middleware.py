81eb93549945ae81e0848894dfc09aaf2fbc8874
Pradeep Verma <pkverma131@gmail.com>
2024-05-27 17:26:17 +0000

    SiteVisitor reporter

--- acwatts_backend/report/middleware.py
+++ acwatts_backend/report/middleware.py
@@ -1,25 +1,29 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
 from .models import SiteVisitor
+import logging
+
+logger = logging.getLogger(__name__)
 
 class SiteVisitorMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        # Check if the current path should be excluded from tracking
-        if request.path_info.startswith('/admin/') or request.path_info in getattr(settings, 'STAT_COUNTER_EXCLUDE_URLS', []):
-            return None
+        try:
+            # Check if the current path should be excluded from tracking
+            if request.path_info.startswith('/admin/') or request.path_info in getattr(settings, 'STAT_COUNTER_EXCLUDE_URLS', []):
+                return None
 
-        # Get client IP address
-        ip_address = self.get_client_ip(request)
+            # Get client IP address
+            ip_address = self.get_client_ip(request)
 
-        # Update or create a SiteVisitor entry for the current path and IP address
-        site_visitor, created = SiteVisitor.objects.get_or_create(path=request.path_info, ip_address=ip_address)
-        site_visitor.hits += 1
-        if created:
-            site_visitor.unique_visits += 1
-        else:
-            if not SiteVisitor.objects.filter(path=request.path_info, ip_address=ip_address).exists():
+            # Update or create a SiteVisitor entry for the current path and IP address
+            site_visitor, created = SiteVisitor.objects.get_or_create(path=request.path_info, ip_address=ip_address)
+            site_visitor.hits += 1
+            if created:
                 site_visitor.unique_visits += 1
-        site_visitor.save()
+            site_visitor.save()
+
+        except Exception as e:
+            logger.error(f"Error processing request: {e}")
 
         # Continue processing the request
         return None
