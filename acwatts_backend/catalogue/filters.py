cbefd8b36f9caad2869ef9013c0c11925f9bd29b
Pradeep Verma <pkverma131@gmail.com>
2024-05-29 09:41:18 +0000

    more filters

--- acwatts_backend/catalogue/filters.py
+++ acwatts_backend/catalogue/filters.py
@@ -7,6 +7,27 @@ class ProductFilter(django_filters.FilterSet):
     min_coverage_area = django_filters.NumberFilter(field_name='important_specification__coverage_area', lookup_expr='gte')
     max_coverage_area = django_filters.NumberFilter(field_name='important_specification__coverage_area', lookup_expr='lte')
 
+    air_conditioner_type = django_filters.CharFilter(field_name='producttospecification__specification__value', lookup_expr='icontains', method='filter_by_specification_label')
+    air_conditioner_capacity = django_filters.CharFilter(field_name='producttospecification__specification__value', lookup_expr='icontains', method='filter_by_specification_label')
+    brand = django_filters.CharFilter(field_name='producttospecification__specification__value', lookup_expr='icontains', method='filter_by_specification_label')
+
+
     class Meta:
         model = Product
         fields = []
+    
+
+    def filter_by_specification_label(self, queryset, name, value):
+        if name == 'air_conditioner_type':
+            label = 'Air Conditioner Type'
+        elif name == 'air_conditioner_capacity':
+            label = 'Air Conditioner Capacity'
+        elif name == 'brand':
+            label = 'Brand'
+        else:
+            return queryset
+        
+        return queryset.filter(
+            producttospecification__specification__label=label,
+            producttospecification__specification__value__icontains=value
+        )
