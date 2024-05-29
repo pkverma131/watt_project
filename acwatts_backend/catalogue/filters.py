4f17f3e3c1119ff9f513d06c9dde72ec4aff58bb
Pradeep Verma <pkverma131@gmail.com>
2024-05-29 08:36:46 +0000

    important spects

--- acwatts_backend/catalogue/filters.py
+++ acwatts_backend/catalogue/filters.py
@@ -2,10 +2,10 @@ import django_filters
 from .models import Product, ImportantSpecification
 
 class ProductFilter(django_filters.FilterSet):
-    min_default_bill_amount = django_filters.NumberFilter(field_name='importantspecification__default_bill_amount', lookup_expr='gte')
-    max_default_bill_amount = django_filters.NumberFilter(field_name='importantspecification__default_bill_amount', lookup_expr='lte')
-    min_coverage_area = django_filters.NumberFilter(field_name='importantspecification__coverage_area', lookup_expr='gte')
-    max_coverage_area = django_filters.NumberFilter(field_name='importantspecification__coverage_area', lookup_expr='lte')
+    min_default_bill_amount = django_filters.NumberFilter(field_name='important_specification__default_bill_amount', lookup_expr='gte')
+    max_default_bill_amount = django_filters.NumberFilter(field_name='important_specification__default_bill_amount', lookup_expr='lte')
+    min_coverage_area = django_filters.NumberFilter(field_name='important_specification__coverage_area', lookup_expr='gte')
+    max_coverage_area = django_filters.NumberFilter(field_name='important_specification__coverage_area', lookup_expr='lte')
 
     class Meta:
         model = Product
